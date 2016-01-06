c6a58cd8650eb8161a50cdd6bd4af1f29f6af9e0
Hai Nguyen <hai427@uw.edu>
2016-01-06 10:44:35 -0800

    finished php warmups

--- php/weather.php
+++ php/weather.php
@@ -1,20 +1,10 @@
 <?php
-
 $json = "http://api.openweathermap.org/data/2.5/weather?q=Seattle,us&units=imperial&appid=2de143494c0b295cca9337e1e96b00e0";
 
-$decode = $var_dump(json_decode($json, true));
+$decode = json_decode(file_get_contents($json), true);
 
-$result = $decode()
+echo '<pre>';
+
+print_r($decode);
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Weather</title>
-</head>
-<body>
-	
-</body>
-</html>
