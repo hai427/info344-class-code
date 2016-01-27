3b384c3c68b0b4ba6206837bc61b4fc66cc653b8
Hai Nguyen <hai427@uw.edu>
2016-01-27 10:20:51 -0800

    HACKA NEWS

---
+++ node-db/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var dbConfig = require('./secret/config-maria.json');
+var bluebird = require('bluebird');
+var connPool = bluebird.promisifyAll(mysql.createPool(dbConfig));
+
+//require our stories controller
+var storiesApi = require('./controllers/stories-api');
+//require our story model
+var Story = require('./models/story.js').Model(connPool);
+
+//create the express application
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/static'));
+
+app.use('/api/v1', storiesApi.Router(Story));
+
+app.listen(80, function() {
+	console.log('server is listening');
+});
+
